ae3a0b3db1e7b70be756db4e844f79548d915766
Paula Ashraf <paula.a.pham@gmail.com>
2024-07-31 14:15:04 +0300

    create functions in home repo

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,1 +1,9 @@
-abstract class HomeRepo {}
+import 'package:dartz/dartz.dart';
+import 'package:dev_zone/core/errors/api_failure.dart';
+import 'package:dev_zone/features/home/data/models/book_model/book_model.dart';
+
+abstract class HomeRepo {
+  Future<Either<ApiFailure, BookModel>> getFeaturedBooks();
+  Future<Either<ApiFailure, BookModel>> getNewestBooks();
+  Future<Either<ApiFailure, BookModel>> getRelatedBooks();
+}
