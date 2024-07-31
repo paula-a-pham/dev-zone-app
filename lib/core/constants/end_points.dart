c0b7cd9d46100ca4a73a7dec2bea82b753b1415d
Paula Ashraf <paula.a.pham@gmail.com>
2024-07-31 15:00:17 +0300

    create constant file for end points and another class for dio helper

---
+++ lib/core/constants/end_points.dart
@@ -0,0 +1,1 @@
+const String kBaseUrl = 'https://www.googleapis.com/books/v1/';
---
+++ lib/core/service/api/dio_helper.dart
@@ -0,0 +1,21 @@
+import 'package:dev_zone/core/constants/end_points.dart';
+import 'package:dio/dio.dart';
+
+class DioHelper {
+  static Dio? dio;
+
+  static void dioInit() {
+    dio = Dio(
+      BaseOptions(
+        baseUrl: kBaseUrl,
+        receiveDataWhenStatusError: true,
+      ),
+    );
+  }
+
+  static Future<Response> getNews(
+      {required String endPoint,
+      required Map<String, dynamic> queryParameters}) async {
+    return await dio!.get(endPoint, queryParameters: queryParameters);
+  }
+}
